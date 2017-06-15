5b56f0cdae0800406b764defed4c85034e2d5066
batu <304701204@qq.com>
2017-06-15 11:18:58 +0800

    增加获取本地硬件信息

---
+++ 泛型/获取本地硬件信息/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 获取本地硬件信息
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
